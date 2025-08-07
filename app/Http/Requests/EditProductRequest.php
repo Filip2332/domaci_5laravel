aa7912b8e68222a4ecff611573ff4b505fcc9d9d
Filip2332 <fikiroki00@gmail.com>
2025-08-07 18:22:33 +0200

    added validation for edit product

---
+++ app/Http/Requests/EditProductRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'amount' => 'required',
+        ];
+    }
+}
